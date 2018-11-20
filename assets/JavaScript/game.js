40717c06a7b9986eedd684291086ba4e916238e4
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2018-11-20 18:53:54 -0500

    next button fix

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -17,7 +17,7 @@
 
   //actual timer function starts when apiseeds gets a song then stops at 0
   function timeLeft() {
-    if (typeof songAnswer !== 'undefined') {
+    if (lyrics.length > 0) {
       $("#timerDisplay").text(timer);
       $("#scoreDisplay").text(score);
       $("#livesDisplay").html("lives" +lives.join(""));
@@ -31,6 +31,8 @@
         $("#livesDisplay").html("lives" +lives.join(""));
         console.log(lives);
       }
+    } else if (lyrics.length = 0) {
+      alert("The game is over! Your final score is: " + score + ". Please refresh to play again!")
     }
 
   };
@@ -68,9 +70,9 @@
   //function to play next song or alert end game
   //timer will only start when the variable songAnswer is not undefined (this variable is from apiseeds.js) so i set it songAnswer to undefined when getting the next song so the timer doesn't prematurially start - or so i though...it still starts - see time left function
   function nextSong() {
+    clearTime();
     songAnswer = 'undefined';
     timerRun = setInterval(timeLeft, 1000)
-    clearTime();
     getSong();
     timeLeft();
   };
